83b1794cd30e552cd9c7fbaa0ca5f769db8d97f5
Hagvan <22118902+Hagvan@users.noreply.github.com>
2025-10-25 19:20:23 +0300

    port fnafcams 3/3

---
+++ Content.Shared/_KS14/SurveillanceCamera/HasMobileCamerasSurveillanceCameraMonitorComponent.cs
@@ -0,0 +1,7 @@
+namespace Content.Shared._KS14.SurveillanceCamera;
+
+// Dummy component for surveillance monitors with known mobile cameras.
+[RegisterComponent]
+public sealed partial class HasMobileCamerasSurveillanceCameraMonitorComponent : Component
+{
+}
