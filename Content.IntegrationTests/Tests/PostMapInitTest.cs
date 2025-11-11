506eaf129a4772ed83934f2137b72ccdc6cc7f1c
lettuce <50937590+whythelettuce@users.noreply.github.com>
2025-11-11 05:04:27 -0800

    Adds (lowpop) Jellyfish Station (#1153)
    * Adds Jellyfish Station
    
    * Adds library sloth, Paperwork
    
    * Updates warden, CMO/HoS offices, firelock decals
    
    * Various tanuko changes
    
    * final cleanup! (i hope)

--- Content.IntegrationTests/Tests/PostMapInitTest.cs
+++ Content.IntegrationTests/Tests/PostMapInitTest.cs
@@ -129,6 +129,9 @@ namespace Content.IntegrationTests.Tests
 
         public static readonly string[] GameMaps =
         {
+            "Spire", // Harmony, Developed by Dogbone10, PenguinCodest, Jack_the_Dragon
+            "Omega", // Harmony, updated from Spanky's version by OrnamentalFreak and CrimsonJupiter
+            "Jellyfish", // Harmony, Developed by whythelettuce
             "Dev",
             "TestTeg",
             "Fland",
