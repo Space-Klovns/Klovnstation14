b9634e215d3942d2e05318aad7578d2fc0324bf1
LaCumbiaDelCoronavirus <90893484+LaCumbiaDelCoronavirus@users.noreply.github.com>
2025-12-25 17:01:37 +0300

    make directionalspriteoffset relative to camera

--- Content.Client/_KS14/DirectionalSpriteOffset/DirectionalSpriteOffsetSystem.cs
+++ Content.Client/_KS14/DirectionalSpriteOffset/DirectionalSpriteOffsetSystem.cs
@@ -1,10 +1,12 @@
 using System.Numerics;
 using Robust.Client.GameObjects;
+using Robust.Client.Graphics;
 
 namespace Content.Client._KS14.DirectionalSpriteOffset;
 
 public sealed class DirectionalSpriteOffsetSystem : EntitySystem
 {
+    [Dependency] private readonly IEyeManager _eyeManager = default!;
     [Dependency] private readonly TransformSystem _transformSystem = default!;
     [Dependency] private readonly SpriteSystem _spriteSystem = default!;
 
@@ -16,8 +18,9 @@ public sealed class DirectionalSpriteOffsetSystem : EntitySystem
 
     public override void Update(float dt)
     {
-        // this could actually be AllEQE but i think that's way too laggy
+        // this could actually be AllEQE to be included for mapping but i think that's way too laggy
         var eqe = EntityQueryEnumerator<DirectionalSpriteOffsetComponent, SpriteComponent>();
+        var eyeRotation = _eyeManager.CurrentEye.Rotation;
 
         while (eqe.MoveNext(out var uid, out var directionalOffsetComponent, out var spriteComponent))
         {
@@ -34,7 +37,7 @@ public sealed class DirectionalSpriteOffsetSystem : EntitySystem
                 worldRotation ??= _transformSystem.GetWorldRotation(Transform(uid), EntityManager.TransformQuery);
 
                 // default to no offset
-                if (!layerData.TryGetValue(layer.EffectiveDirection(layer.ActualState!, worldRotation.Value, overrideDirection: null), out var directionalOffset))
+                if (!layerData.TryGetValue(layer.EffectiveDirection(layer.ActualState!, worldRotation.Value + eyeRotation, overrideDirection: null), out var directionalOffset))
                     directionalOffset = Vector2.Zero;
 
                 _spriteSystem.LayerSetOffset(layer, directionalOffset);
