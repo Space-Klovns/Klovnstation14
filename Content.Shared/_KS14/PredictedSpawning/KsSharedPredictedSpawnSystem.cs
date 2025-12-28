7ec9f139f321c6fe81bab0837564334c253c047f
LaCumbiaDelCoronavirus <90893484+LaCumbiaDelCoronavirus@users.noreply.github.com>
2025-12-28 16:32:24 +0300

    GUH

--- Content.Shared/_KS14/PredictedSpawning/KsSharedPredictedSpawnSystem.cs
+++ Content.Shared/_KS14/PredictedSpawning/KsSharedPredictedSpawnSystem.cs
@@ -3,6 +3,8 @@ using Robust.Shared.Prototypes;
 
 namespace Content.Shared._KS14.PredictedSpawning;
 
+// TODO LCDC: MAKE THIS ACTUALLY WORK GEEEG
+
 /// <summary>
 ///     Contains replacements for <see cref="EntityManager.PredictedSpawn(string?, Robust.Shared.Prototypes.ComponentRegistry?, bool)"/>
 ///         and it's million variants, because RT's implementation is very bad. 
@@ -13,16 +15,30 @@ public abstract class KsSharedPredictedSpawnSystem : EntitySystem
     ///     This has a very slight overhead of adding <see cref="KsPredictedSpawnComponent"/>
     ///         to the entity.
     /// </remarks>
+    // public EntityUid PredictedSpawn(string entityProtoId, ComponentRegistry? componentOverrides = null, bool doMapInit = false)
+    //     => FlagPredictedAndReturn(Spawn(entityProtoId, overrides: componentOverrides, doMapInit: doMapInit));
+
+    // TODO LCDC: FIX THIS SHIT
     public EntityUid PredictedSpawn(string entityProtoId, ComponentRegistry? componentOverrides = null, bool doMapInit = false)
-        => FlagPredictedAndReturn(Spawn(entityProtoId, overrides: componentOverrides, doMapInit: doMapInit));
+        => EntityManager.PredictedSpawn(entityProtoId, overrides: componentOverrides, doMapInit: doMapInit);
 
     /// <inheritdoc cref="PredictedSpawn(string, ComponentRegistry?, bool)"/>
+    // public EntityUid PredictedSpawn(string entityProtoId, MapCoordinates coordinates, ComponentRegistry? componentOverrides = null, Angle rotation = default)
+    //     => FlagPredictedAndReturn(Spawn(entityProtoId, coordinates, overrides: componentOverrides, rotation: rotation));
+
+    // TODO LCDC: FIX THIS SHIT
     public EntityUid PredictedSpawn(string entityProtoId, MapCoordinates coordinates, ComponentRegistry? componentOverrides = null, Angle rotation = default)
-        => FlagPredictedAndReturn(Spawn(entityProtoId, coordinates, overrides: componentOverrides, rotation: rotation));
+        => EntityManager.PredictedSpawn(entityProtoId, coordinates, overrides: componentOverrides, rotation: rotation);
+
 
     /// <inheritdoc cref="PredictedSpawn(string, ComponentRegistry?, bool)"/>
+    // public new EntityUid PredictedSpawnAttachedTo(string entityProtoId, EntityCoordinates coordinates, ComponentRegistry? componentOverrides = null, Angle rotation = default)
+    //     => FlagPredictedAndReturn(SpawnAttachedTo(entityProtoId, coordinates, overrides: componentOverrides, rotation: rotation));
+
+    // TODO LCDC: FIX THIS SHIT
     public new EntityUid PredictedSpawnAttachedTo(string entityProtoId, EntityCoordinates coordinates, ComponentRegistry? componentOverrides = null, Angle rotation = default)
-        => FlagPredictedAndReturn(SpawnAttachedTo(entityProtoId, coordinates, overrides: componentOverrides, rotation: rotation));
+        => EntityManager.PredictedSpawnAttachedTo(entityProtoId, coordinates, overrides: componentOverrides, rotation: rotation);
+
 
     /// <summary>
     ///     Flags the given entity as predicted.
