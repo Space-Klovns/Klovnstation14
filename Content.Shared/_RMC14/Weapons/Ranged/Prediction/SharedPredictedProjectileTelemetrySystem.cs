b3db04775517aca8f694feddb9a34eb6c617897e
nabegator220 <nabe@gator.lol>
2026-01-05 18:51:09 +0100

    deleted superfluous rmc saars

--- Content.Shared/_RMC14/Weapons/Ranged/Prediction/SharedPredictedProjectileTelemetrySystem.cs
+++
@@ -1,6 +0,0 @@
-// Telemetry removed: use RMC telemetry/cvars instead.
-// Placeholder system kept to avoid breaking references while tidying.
-namespace Content.Shared._RMC14.Weapons.Ranged.Prediction
-{
-    internal sealed class RemovedPredictedProjectileTelemetrySystem { }
-}
