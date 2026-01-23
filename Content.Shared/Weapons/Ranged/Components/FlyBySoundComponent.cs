27667783061c14a692f7b81af029fd9a6541ccd7
LaCumbiaDelCoronavirus <90893484+LaCumbiaDelCoronavirus@users.noreply.github.com>
2026-01-23 20:28:00 +0800

    fix mpl

--- Content.Shared/Weapons/Ranged/Components/FlyBySoundComponent.cs
+++ Content.Shared/Weapons/Ranged/Components/FlyBySoundComponent.cs
@@ -4,7 +4,7 @@
 // SPDX-FileCopyrightText: 2023 Kara
 // SPDX-FileCopyrightText: 2026 LaCumbiaDelCoronavirus
 //
-// SPDX-License-Identifier: MPL-2.0
+// SPDX-License-Identifier: MIT
 
 using Robust.Shared.Audio;
 using Robust.Shared.GameStates;
