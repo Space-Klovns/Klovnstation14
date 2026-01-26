7e226d6e4cda359c68ece0b347d4144c01367628
nabegator220 <nabe@gator.lol>
2026-01-26 20:14:01 +0100

    Revert "ez (#150)"
    This reverts commit 826ae61e6afc7631e356a27ca001359290f87e2e.

--- Content.Shared/_KS14/ComplexShove/ComplexShoveSystem.cs
+++ Content.Shared/_KS14/ComplexShove/ComplexShoveSystem.cs
@@ -1,6 +1,4 @@
-// SPDX-FileCopyrightText: 2026 FrauzJ
 // SPDX-FileCopyrightText: 2026 LaCumbiaDelCoronavirus
-// SPDX-FileCopyrightText: 2026 github_actions[bot]
 //
 // SPDX-License-Identifier: MPL-2.0
 
@@ -94,7 +92,7 @@ public sealed class ComplexShoveSystem : EntitySystem
 
         _staminaSystem.TakeStaminaDamage(
             shovedEntity.Owner,
-            shovedEntity.Comp.CritThreshold * shoverEntity.Comp.BasicShoveStaminaDamageFraction / 6.5f,
+            shovedEntity.Comp.CritThreshold * shoverEntity.Comp.BasicShoveStaminaDamageFraction,
             component: shovedEntity.Comp,
             source: shoverEntity,
             ignoreResist: true
