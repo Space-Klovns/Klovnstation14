d8651d2b7e798027a3683dbde015e49689f5bb3b
nabegator220 <nabe@gator.lol>
2026-02-02 16:12:08 +0100

    added system, tag functionality untested

---
+++ Content.Shared/_KS14/Held/Components/HeldGrantComponentComponent.cs
@@ -0,0 +1,15 @@
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared._KS14.Held.Components // KS14 - this is an analogue to the goobstation clothinggrantsystem that we already ported
+{
+    [RegisterComponent]
+    public sealed partial class HeldGrantComponentComponent : Component
+    {
+        [DataField("component", required: true)]
+        [AlwaysPushInheritance]
+        public ComponentRegistry Components { get; private set; } = new();
+
+        [ViewVariables(VVAccess.ReadWrite)]
+        public Dictionary<string, bool> Active = new();
+    }
+}
