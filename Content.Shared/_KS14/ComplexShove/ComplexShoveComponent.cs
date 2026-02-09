2694226f7dbe78fbd8d771a6d0b8effd0a2d038b
FrauzJ <mihailpavlichenko2006@gmail.com>
2026-02-09 02:05:53 +0200

    dfsdgfsd

--- Content.Shared/_KS14/ComplexShove/ComplexShoveComponent.cs
+++ Content.Shared/_KS14/ComplexShove/ComplexShoveComponent.cs
@@ -46,7 +46,7 @@ public sealed partial class ComplexShoveComponent : Component
     ///         should be applied.
     /// </summary>
     [DataField, ViewVariables(VVAccess.ReadWrite)]
-    public long WallshoveCollisionMask = (long)CollisionGroup.MidImpassable;
+    public long WallshoveCollisionMask = (long)CollisionGroup.Impassable;
 
     /// <summary>
     ///     Push force applied to targets who are standing.
