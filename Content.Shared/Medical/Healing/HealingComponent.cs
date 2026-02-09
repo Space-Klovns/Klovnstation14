dffed3af2aa745ae8f1fb90301be1f5411dc64ee
DeusMaldPr <214400248+DeusMaldPr@users.noreply.github.com>
2026-02-09 23:11:06 +0100

    goida

--- Content.Shared/Medical/Healing/HealingComponent.cs
+++ Content.Shared/Medical/Healing/HealingComponent.cs
@@ -50,13 +50,13 @@ public sealed partial class HealingComponent : Component
     /// How long it takes to apply the damage.
     /// </summary>
     [DataField, AutoNetworkedField]
-    public TimeSpan Delay = TimeSpan.FromSeconds(3f);
+    public TimeSpan Delay = TimeSpan.FromSeconds(2f); //klovn, was 2f
 
     /// <summary>
     /// Delay multiplier when healing yourself.
     /// </summary>
     [DataField, AutoNetworkedField]
-    public float SelfHealPenaltyMultiplier = 3f;
+    public float SelfHealPenaltyMultiplier = 2f; // klovn, was 3f, changed for gameplay reasons
 
     /// <summary>
     /// Sound played on healing begin.
