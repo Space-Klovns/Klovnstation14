792cd6ff9beca3cb0e04aa0ee3e6c980f4bb573a
LaCumbiaDelCoronavirus <90893484+LaCumbiaDelCoronavirus@users.noreply.github.com>
2026-02-16 02:35:22 +0800

    z

---
+++ Content.Shared/_KS14/Speczones/AlwaysAllowedInSpeczoneComponent.cs
@@ -0,0 +1,12 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._KS14.Speczones;
+
+/// <summary>
+///     Added to things whose use gets blocked when in speczones,
+///         so as to allow them to always be used in speczones.
+///
+///     Intended to be manually added by admins.
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+public abstract partial class AlwaysAllowedInSpeczoneComponent : Component;
