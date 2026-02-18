608dac6535b9bbf0729847b62df77937769e8b64
nabegator220 <nabe@gator.lol>
2026-02-18 15:23:26 +0100

    zakony fixed

--- Content.Shared/Silicons/Borgs/BorgTypePrototype.cs
+++ Content.Shared/Silicons/Borgs/BorgTypePrototype.cs
@@ -5,6 +5,7 @@ using Content.Shared.Silicons.Borgs.Components;
 using Content.Shared.Whitelist;
 using Robust.Shared.Audio;
 using Robust.Shared.Prototypes;
+using Content.Shared.Silicons.Laws; //KS14
 
 namespace Content.Shared.Silicons.Borgs;
 
@@ -151,4 +152,12 @@ public sealed partial class BorgTypePrototype : IPrototype
     /// </summary>
     [DataField]
     public SoundSpecifier FootstepCollection { get; set; } = new SoundCollectionSpecifier(DefaultFootsteps);
+
+    /// KS14 -Laws
+
+    /// <summary>
+    /// KS14 - do we have a specific lawset that we want to use for this borg?
+    /// </summary>
+    [DataField]
+    public ProtoId<SiliconLawsetPrototype> Laws = string.Empty;
 }
