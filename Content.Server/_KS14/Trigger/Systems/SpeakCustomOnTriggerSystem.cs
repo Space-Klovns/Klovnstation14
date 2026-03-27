6418480ec4f6777c654ede2247080384b3d87f3a
nabegator220 <nabe@gator.lol>
2026-03-27 17:01:04 +0100

    removed artifact

--- Content.Server/_KS14/Trigger/Systems/SpeakCustomOnTriggerSystem.cs
+++
@@ -1,7 +0,0 @@
-// SPDX-FileCopyrightText: 2025 DrSmugleaf
-// SPDX-FileCopyrightText: 2025 Princess Cheeseballs
-// SPDX-FileCopyrightText: 2025 Qerd
-// SPDX-FileCopyrightText: 2025 Tayrtahn
-// SPDX-FileCopyrightText: 2026 nabegator220
-//
-// SPDX-License-Identifier: MPL-2.0
