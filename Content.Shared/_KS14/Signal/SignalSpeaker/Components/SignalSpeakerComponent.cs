7141fc8c45bf75853e3574e083f5f3914e09b6fe
nabegator220 <nabe@gator.lol>
2026-04-06 17:14:51 +0200

    stash

---
+++ Content.Shared/_KS14/Signal/SignalSpeaker/Components/SignalSpeakerComponent.cs
@@ -0,0 +1,35 @@
+using Content.Shared.Whitelist;
+using Robust.Shared.GameStates;
+using Robust.Shared.Serialization;
+using Content.Shared._KS14.Signal.SignalSpeaker.EntitySystems;
+
+namespace Content.Shared._KS14.Signal.SignalSpeaker.Components;
+
+[RegisterComponent, NetworkedComponent]
+[Access(typeof(SharedSignalSpeakerSystem))]
+public sealed partial class SignalSpeakerComponent : Component
+{
+    [ViewVariables(VVAccess.ReadWrite), Access(Other = AccessPermissions.ReadWriteExecute)]
+    [DataField]
+    public string AssignedText = string.Empty;
+
+    [ViewVariables(VVAccess.ReadWrite)]
+    [DataField]
+    public int MaxTextChars = 50;
+
+    [ViewVariables(VVAccess.ReadWrite)]
+    [DataField]
+    public EntityWhitelist? Whitelist;
+
+    [ViewVariables(VVAccess.ReadWrite)]
+    [DataField]
+    public EntityWhitelist? Blacklist;
+}
+
+[Serializable, NetSerializable]
+public sealed class SignalSpeakerComponentState(string assignedText) : IComponentState
+{
+    public string AssignedText = assignedText;
+
+    public int MaxTextChars;
+}
