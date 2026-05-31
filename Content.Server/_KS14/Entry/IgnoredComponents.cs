1031d705525676ef5a5978c52b5696e1cc81055a
LaCumbiaDelCoronavirus <90893484+LaCumbiaDelCoronavirus@users.noreply.github.com>
2026-05-31 23:54:28 +0800

    z

--- Content.Server/_KS14/Entry/IgnoredComponents.cs
+++ Content.Server/_KS14/Entry/IgnoredComponents.cs
@@ -4,6 +4,6 @@ public static class KsIgnoredComponents
 {
     public static string[] List => [
         "DirectionalSpriteManipulation",
-        "KsWaveDistortionComponent"
+        "KsWaveDistortion"
     ];
 }
