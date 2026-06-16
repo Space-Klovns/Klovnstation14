2dd663e398abfdcf1f7db212df7f337f3b259aa8
LaCumbiaDelCoronavirus <90893484+LaCumbiaDelCoronavirus@users.noreply.github.com>
2026-06-17 03:50:36 +0800

    uh

--- Content.Client/_KS14/Trail/KsTrailOverlay.cs
+++ Content.Client/_KS14/Trail/KsTrailOverlay.cs
@@ -3,22 +3,16 @@ using System.Linq;
 using System.Numerics;
 using Content.Client.Graphics;
 using Content.Shared._KS14.SupplyPod;
+using Content.Shared._KS14.Trail;
 using Robust.Client.GameObjects;
 using Robust.Client.Graphics;
 using Robust.Client.ResourceManagement;
 using Robust.Shared.Enums;
 
-namespace Content.Client._KS14.SupplyPod;
+namespace Content.Client._KS14.Trail;
 
-/*
-    Please another overlay that just draws entities oh boy
-*/
-
-public sealed class SupplyPodOverlay : Overlay
+public sealed class KsTrailOverlay : Overlay
 {
-    private readonly ShaderInstance _maskShader;
-    private readonly ShaderInstance _drawShader;
-
     [Dependency] private readonly IEntityManager _entityManager = default!;
     [Dependency] private readonly IResourceCache _resourceCache = default!;
     [Dependency] private readonly IClyde _clyde = default!;
@@ -26,7 +20,7 @@ public sealed class SupplyPodOverlay : Overlay
 
     public override OverlaySpace Space => OverlaySpace.WorldSpaceEntities;
 
-    public const int ConstZIndex = (int)Shared.DrawDepth.DrawDepth.LargeObjects; // Under ghosts and fire, above mostly everything else
+    public const int ConstZIndex = (int)Shared.DrawDepth.DrawDepth.Effects;
     private const float Scale = 1f;
     private static readonly Matrix3x2 ScaleMatrix = Matrix3Helpers.CreateScale(new Vector2(Scale, Scale));
     public static readonly Vector2 HalfNegativeVector2PerPixel = new(-0.5f / EyeManager.PixelsPerMeter, -0.5f / EyeManager.PixelsPerMeter);
@@ -34,11 +28,8 @@ public sealed class SupplyPodOverlay : Overlay
     private OverlayResourceCache<OverlayResources> _resources = new();
     private readonly List<(SupplyPodDoorDrawerComponent, Matrix3x2)> _drawDataCache = new();
 
-    public SupplyPodOverlay(ShaderInstance stencilMaskShader, ShaderInstance stencilDrawShader)
+    public KsTrailOverlay(ShaderInstance stencilMaskShader, ShaderInstance stencilDrawShader)
     {
-        _maskShader = stencilMaskShader;
-        _drawShader = stencilDrawShader;
-
         ZIndex = ConstZIndex;
     }
 
@@ -68,47 +59,23 @@ public sealed class SupplyPodOverlay : Overlay
 
         var invMatrix = renderTarget.GetWorldToLocalMatrix(viewport.Eye!, scale);
 
-        worldHandle.RenderInRenderTarget(resources.MaskTarget, () =>
+        var eqe = _entityManager.EntityQueryEnumerator<KsTrailComponent, TransformComponent>();
+        while (eqe.MoveNext(out var trailComponent, out var transformComponent))
         {
-            var eqe = _entityManager.EntityQueryEnumerator<SupplyPodDoorDrawerComponent, TransformComponent>();
-            while (eqe.MoveNext(out var supplyPodComponent, out var transformComponent))
-            {
-                if (!TryGetTexture(supplyPodComponent.DoorData, out var doorTexture))
-                    continue;
+            var invGridMatrix = _transformSystem.GetWorldMatrix(transformComponent);
 
-                var invGridMatrix = _transformSystem.GetWorldMatrix(transformComponent);
+            // var scaledWorld = Matrix3x2.Multiply(ScaleMatrix, Matrix3Helpers.CreateTranslation(_transformSystem.GetWorldPosition(transformComponent)));
+            // var worldMatrix = Matrix3x2.Multiply(rotationMatrix, scaledWorld);
+            // // Apply the inverse matrix to transform to render target space. otherwise, we would be rendering in worldspace
+            // var renderTargetMatrix = Matrix3x2.Multiply(worldMatrix, invMatrix);
+            // worldHandle.SetTransform(invGridMatrix);
 
-                // var scaledWorld = Matrix3x2.Multiply(ScaleMatrix, Matrix3Helpers.CreateTranslation(_transformSystem.GetWorldPosition(transformComponent)));
-                // var worldMatrix = Matrix3x2.Multiply(rotationMatrix, scaledWorld);
-                // // Apply the inverse matrix to transform to render target space. otherwise, we would be rendering in worldspace
-                // var renderTargetMatrix = Matrix3x2.Multiply(worldMatrix, invMatrix);
-                worldHandle.SetTransform(invGridMatrix);
+            // var offset = HalfNegativeVector2PerPixel * doorTexture.Size;
+            // worldHandle.DrawTexture(doorTexture, offset, modulate: Color.Black);
 
-                var offset = HalfNegativeVector2PerPixel * doorTexture.Size;
-                worldHandle.DrawTexture(doorTexture, offset, modulate: Color.Black);
-
-                _drawDataCache.Add((supplyPodComponent, invGridMatrix));
-            }
-        }, Color.White);
-
-        if (_drawDataCache.Count == 0)
-            goto end;
-
-        worldHandle.UseShader(_maskShader);
-        worldHandle.DrawTextureRect(resources.MaskTarget.Texture, args.WorldBounds);
-
-        worldHandle.UseShader(_drawShader);
-        foreach (var drawDatum in _drawDataCache)
-        {
-            if (!TryGetTexture(drawDatum.Item1.DecalData, out var decalTexture))
-                continue;
-
-            worldHandle.SetTransform(drawDatum.Item2);
-            worldHandle.DrawTexture(decalTexture, HalfNegativeVector2PerPixel * decalTexture.Size);
+            // _drawDataCache.Add((supplyPodComponent, invGridMatrix));
         }
 
-        _drawDataCache.Clear();
-    end:
         worldHandle.SetTransform(Matrix3x2.Identity);
         worldHandle.UseShader(null);
     }
---
+++ Content.Server/_KS14/SupplyPod/SupplyPodSystem.cs
@@ -0,0 +1,11 @@
+using Content.Shared._KS14.SupplyPod;
+
+namespace Content.Server._KS14.SupplyPod;
+
+/// <summary>
+///     Kept you waiting, huh?
+/// </summary>
+public sealed class SupplyPodSystem : SharedSupplyPodSystem
+{
+    
+}
