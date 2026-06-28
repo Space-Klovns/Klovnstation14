230b914aed01b2dd6e6feb32f4b33ed900a4fe52
LaCumbiaDelCoronavirus <90893484+LaCumbiaDelCoronavirus@users.noreply.github.com>
2026-06-28 22:56:09 +0800

    add random rotation to gridspawner

--- Content.Server/_KS14/GridSpawner/KsGridSpawnerComponent.cs
+++ Content.Server/_KS14/GridSpawner/KsGridSpawnerComponent.cs
@@ -24,6 +24,16 @@ public sealed partial class KsGridSpawnerComponent : Component, ISerializationHo
 
     /// <summary>
     ///     If not null, the X coordinate will act as the minimum
+    ///         and Y coordinate as maximum, for the random angle
+    ///         that the spawned grid is rotated at. This is added onto <see cref="Rotation"/>
+    ///         because i didn't feel like obsoleting it.
+    /// </summary>
+    [DataField]
+    [ViewVariables(VVAccess.ReadWrite)]
+    public Vector2d? RotationRangeDeg = null;
+
+    /// <summary>
+    ///     If not null, the X coordinate will act as the minimum
     ///         and Y coordinate as maximum, for the random distance
     ///         that the spawned grid is from the spawner.
     /// </summary>
@@ -37,13 +47,22 @@ public sealed partial class KsGridSpawnerComponent : Component, ISerializationHo
 
     void ISerializationHooks.AfterDeserialization()
     {
-        if (SpawnRange is not { } spawnRange)
-            return;
+        if (SpawnRange is { } spawnRange)
+        {
+            if (spawnRange.X > spawnRange.Y)
+                throw new ArgumentException("SpawnRange.X (minimum range) must not be higher than SpawnRange.Y (maximum range)!");
 
-        if (spawnRange.X > spawnRange.Y)
-            throw new ArgumentException("SpawnRange.X (minimum range) must not be higher than SpawnRange.Y (maximum range)!");
+            if (spawnRange.Y < 0f)
+                throw new ArgumentException("Neither component of SpawnRange may be negative!");
+        }
 
-        if (spawnRange.Y < 0f)
-            throw new ArgumentException("Neither component of SpawnRange may be negative!");
+        if (RotationRangeDeg is { } rotationRange)
+        {
+            if (rotationRange.X > rotationRange.Y)
+                throw new ArgumentException("RotationRangeDeg.X (minimum range) must not be higher than RotationRangeDeg.Y (maximum range)!");
+
+            if (rotationRange.Y < 0f)
+                throw new ArgumentException("Neither component of RotationRangeDeg may be negative!");
+        }
     }
 }
