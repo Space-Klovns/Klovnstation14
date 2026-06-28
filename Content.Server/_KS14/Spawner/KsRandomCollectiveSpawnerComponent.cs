502e4e824a87e4c02141fdd80e842dacafe2c806
LaCumbiaDelCoronavirus <90893484+LaCumbiaDelCoronavirus@users.noreply.github.com>
2026-06-28 23:01:57 +0800

    ez

--- Content.Server/_KS14/Spawner/KsRandomCollectiveSpawnerComponent.cs
+++ Content.Server/_KS14/Spawner/KsRandomCollectiveSpawnerComponent.cs
@@ -2,6 +2,11 @@ using Robust.Shared.Prototypes;
 
 namespace Content.Server._KS14.Spawner;
 
+/// <summary>
+///     When added to a grid/map/whatever that is a valid scope (see <see cref="Scope"/>),
+///         upon mapinit of that scope, one randomly picked spawner for each entity prototype added
+///         will spawn the given entity. After this process is completed, all spawners of that scope are deleted.
+/// </summary>
 [RegisterComponent]
 [Access(typeof(KsRandomCollectiveSpawnerSystem))]
 public sealed partial class KsRandomCollectiveSpawnerComponent : Component
@@ -9,6 +14,9 @@ public sealed partial class KsRandomCollectiveSpawnerComponent : Component
     [DataField(required: true)]
     public EntProtoId ProtoId;
 
+    /// <summary>
+    ///     Whether this only works on grids or on maps.
+    /// </summary>
     [DataField, ViewVariables(VVAccess.ReadOnly)]
     public KsRandomCollectiveSpawnScope Scope = KsRandomCollectiveSpawnScope.Grid;
 }
@@ -16,6 +24,5 @@ public sealed partial class KsRandomCollectiveSpawnerComponent : Component
 public enum KsRandomCollectiveSpawnScope : byte
 {
     Grid,
-    Map,
-    Global
+    Map
 }
--- Content.Server/_KS14/Spawner/KsRandomCollectiveSpawnerSystem.cs
+++ Content.Server/_KS14/Spawner/KsRandomCollectiveSpawnerSystem.cs
@@ -1,40 +1,71 @@
-
-using Content.Shared.GameTicking;
+using Robust.Shared.Random;
+using Robust.Shared.Utility;
 
 namespace Content.Server._KS14.Spawner;
 
 public sealed class KsRandomCollectiveSpawnerSystem : EntitySystem
 {
-    private HashSet<EntityUid> _globalCache = [];
+    [Dependency] private readonly IRobustRandom _robustRandom = default!;
 
     public override void Initialize()
     {
         base.Initialize();
+
+        SubscribeLocalEvent<KsRandomCollectiveSpawnerComponent, ComponentStartup>(OnSpawnerStartup);
+        SubscribeLocalEvent<KsRandomCollectiveSpawnerComponent, ComponentShutdown>(OnSpawnerShutdown);
+        SubscribeLocalEvent<KsRandomCollectiveScopeComponent, MapInitEvent>(OnScopeMapInit);
     }
 
-    private HashSet<EntityUid> GetOrAddCache(TransformComponent transformComponent, KsRandomCollectiveSpawnerComponent spawnerComponent)
+    private EntityUid? GetScope(Entity<KsRandomCollectiveSpawnerComponent, TransformComponent> entity)
     {
-        if ()
+        var scopeUid = entity.Comp1.Scope switch
+        {
+            KsRandomCollectiveSpawnScope.Grid => entity.Comp2.GridUid,
+            KsRandomCollectiveSpawnScope.Map => entity.Comp2.MapUid,
+            _ => throw new InvalidOperationException($"Invalid scope for {nameof(KsRandomCollectiveSpawnerComponent)}: {entity.Comp1.Scope}")
+        };
+
+        return scopeUid;
     }
 
-    private void OnRoundRestartCleanup(RoundRestartCleanupEvent args)
+    private void OnSpawnerStartup(Entity<KsRandomCollectiveSpawnerComponent> entity, ref ComponentStartup args)
     {
-        _globalCache.Clear();
+        if (GetScope((entity, entity, Transform(entity))) is not { } scopeUid)
+            return;
 
+        // linq final boss without being linq
+        EnsureComp<KsRandomCollectiveScopeComponent>(scopeUid).Spawners.GetOrNew(entity.Comp.ProtoId).Add(entity);
     }
 
-    private void OnStartup(Entity<KsRandomCollectiveSpawnerComponent> entity, ref ComponentStartup args)
+    private void OnSpawnerShutdown(Entity<KsRandomCollectiveSpawnerComponent> entity, ref ComponentShutdown args)
     {
+        if (!EntityManager.TransformQuery.TryGetComponent(entity, out var transformComponent) ||
+            GetScope((entity, entity, transformComponent)) is not { } scopeUid ||
+            !TryComp<KsRandomCollectiveScopeComponent>(entity, out var scopeComponent) ||
+            !scopeComponent.Spawners.TryGetValue(entity.Comp.ProtoId, out var cache))
+            return;
 
+        cache.Remove(entity);
     }
 
-    private void OnShutdown(Entity<KsRandomCollectiveSpawnerComponent> entity, ref ComponentShutdown args)
+    private void OnScopeMapInit(Entity<KsRandomCollectiveScopeComponent> entity, ref MapInitEvent args)
     {
+        if (entity.Comp.Spawners.Count == 0)
+            goto fuck;
 
-    }
+        foreach (var (spawnedEntProtoId, spawnerUids) in entity.Comp.Spawners)
+        {
+            if (spawnerUids.Count == 0)
+                continue;
 
-    private void OnMapInit()
-    {
+            var spawnCoordinates = Transform(_robustRandom.Pick(spawnerUids)).Coordinates;
+            foreach (var otherSpawnerUid in spawnerUids)
+                QueueDel(otherSpawnerUid);
 
+            Spawn(spawnedEntProtoId, spawnCoordinates);
+        }
+
+    fuck:
+        RemComp(entity, entity.Comp);
     }
 }
