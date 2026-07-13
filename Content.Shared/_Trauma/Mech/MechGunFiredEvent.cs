Kenkaitos/Trauma-Station

mech system cleanup and fixing (#1691)
Co-authored-by: deltanedas <@deltanedas:kde.org>

Content.Trauma.Common/Mech/MechGunFiredEvent.cs
@@ -1,0 +1,9 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace Content.Trauma.Common.Mech;
+
+/// <summary>
+/// Event raised on a mech gun equipment entity after it has been fired.
+/// </summary>
+[ByRefEvent]
+public record struct MechGunFiredEvent();

Space-Klovns/Klovnstation14

Content.Shared/_Trauma/Mech/MechGunFiredEvent.cs
@@ -1,0 +1,9 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace Content.Shared._Trauma.Mech;
+
+/// <summary>
+/// Event raised on a mech gun equipment entity after it has been fired.
+/// </summary>
+[ByRefEvent]
+public record struct MechGunFiredEvent();