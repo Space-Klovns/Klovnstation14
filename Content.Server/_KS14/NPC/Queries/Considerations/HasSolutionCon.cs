af63d0d3124021c072d5cd1c3e94b80e359f2261
LaCumbiaDelCoronavirus <90893484+LaCumbiaDelCoronavirus@users.noreply.github.com>
2026-07-24 22:12:48 +0800

    update HasSolutionCon to modern standards

--- Content.Server/_KS14/NPC/Queries/Considerations/HasSolutionCon.cs
+++ Content.Server/_KS14/NPC/Queries/Considerations/HasSolutionCon.cs
@@ -1,15 +1,15 @@
 using Content.Server.NPC;
 using Content.Server.NPC.Queries.Considerations;
+using Content.Shared.Chemistry.Components;
 using Content.Shared.Chemistry.Components.SolutionManager;
 
 namespace Content.Server._KS14.NPC.Queries.Considerations;
 
 public sealed partial class HasSolutionCon : UtilityConsideration
 {
-    [Dependency] private EntityQuery<SolutionContainerManagerComponent> _solutionContainerQuery = default!;
+    [Dependency] private EntityQuery<SolutionManagerComponent> _solutionManagerQuery = default!;
+    [Dependency] private EntityQuery<SolutionComponent> _solutionQuery = default!;
 
     public override float GetScore(NPCBlackboard blackboard, EntityUid ownerUid, EntityUid targetUid)
-    {
-        return _solutionContainerQuery.HasComponent(targetUid) ? 1f : 0f;
-    }
+        => (_solutionManagerQuery.HasComponent(targetUid) || _solutionQuery.HasComponent(targetUid)) ? 1f : 0f;
 }
