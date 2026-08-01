cd3daf83d669570db778d56fd1b011e24148677e
LaCumbiaDelCoronavirus <90893484+LaCumbiaDelCoronavirus@users.noreply.github.com>
2026-08-01 20:06:41 +0800

    add backblast, and at-4

---
+++ Content.Shared/_KS14/Weapons/Ranged/GunBackblastComponent.cs
@@ -0,0 +1,57 @@
+using Content.Shared.Damage;
+using Content.Shared.Physics;
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._KS14.Weapons.Ranged;
+
+// damage intentionally not dependent on distance
+
+/// <summary>
+///    Makes a literal cone of backblast when firing this ammo, on the shooter/gun.
+///         This is added to the affected gun.
+///
+///     The area of effect is a circular sector originating from the shooter, of
+///         <see cref="Radius"/> metres radius, with an angle of <see cref="EffectField"/>.
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+public sealed partial class GunBackblastComponent : Component
+{
+    #region Cone
+
+    /// <summary>
+    ///     Direction of backblast relative to firing direction. This is
+    ///         directly added onto firing direction.
+    /// </summary>
+    [DataField]
+    public Angle DirectionOffset = Angle.Zero;
+
+    /// <summary>
+    ///     Radius of the backblast - if your distance from the shooter
+    ///         is farther than this, you won't get hit.
+    /// </summary>
+    [DataField(required: true)]
+    public float Radius = 0f;
+
+    /// <summary>
+    ///     Angle of the circular sector of effect.
+    /// </summary>
+    [DataField]
+    public Angle EffectField = 0f;
+
+    #endregion
+    #region Damage
+
+    [DataField]
+    public DamageSpecifier Damage = new();
+
+    [DataField]
+    public float PushForce = 100f;
+
+    [DataField]
+    public TimeSpan KnockdownTime = TimeSpan.Zero;
+
+    #endregion
+
+    [DataField]
+    public CollisionGroup CollisionGroup = CollisionGroup.BulletImpassable;
+}
