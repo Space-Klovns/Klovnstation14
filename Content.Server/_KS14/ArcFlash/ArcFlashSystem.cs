c876a5accab6421e96dc8f4b3da3616d54dae671
nabegator220 <nabe@gator.lol>
2026-08-21 18:29:13 +0200

    Move N14 mapping additions to separate branch

--- Content.Server/_KS14/ArcFlash/ArcFlashSystem.cs
+++ Content.Server/_KS14/ArcFlash/ArcFlashSystem.cs
@@ -7,7 +7,7 @@ using Content.Shared._KS14.ArcFlash.Components;
 
 namespace Content.Server._KS14.ArcFlash;
 
-public sealed partial class ArcFlashSystem : EntitySystem
+public sealed partial class MindShieldSystem : EntitySystem
 {
     [Dependency] private ElectrocutionSystem _electrocutionSystem = default!;
     [Dependency] private LightningSystem _lightning = default!;
