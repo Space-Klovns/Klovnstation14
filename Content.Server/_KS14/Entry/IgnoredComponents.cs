b87f089c09e62742823752e660a97844a3188d16
LaCumbiaDelCoronavirus <90893484+LaCumbiaDelCoronavirus@users.noreply.github.com>
2026-09-05 21:55:44 +0800

    casimera, casimera

--- Content.Server/_KS14/Entry/IgnoredComponents.cs
+++ Content.Server/_KS14/Entry/IgnoredComponents.cs
@@ -6,6 +6,7 @@ public static class KsIgnoredComponents
         "DirectionalSpriteManipulation",
         "KsWaveDistortion",
         "KsShadow",
-        "KsRcdPlacementNoHint"
+        "KsRcdPlacementNoHint",
+        "KsAlwaysDisplaced"
     ];
 }
